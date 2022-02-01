19efec227a3736e6f94665b01d1f880f446f1faa
Victor Avila <vicav99@gmail.com>
2022-02-01 10:32:19 -0700

    feat: add react router with initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { Fab } from '@mui/material';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { ScrollTop, Toolbar } from './components';
+import * as Pages from './pages';
 
 
 function App() {
   return (
     <>
-      <Toolbar />
+      <BrowserRouter>
+        <Toolbar id="back-to-top-anchor" />
+        <Routes>
+          <Route path="/" element={<Pages.Home />} />
+          <Route path="cart" element={<Pages.Cart />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
       <ScrollTop>
         <Fab color="secondary" size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon />
